67c9d0f468e275d44e06975d0e1956c0fc3bc0fc
Manuel Díaz <yosoymacnolo@gmail.com>
2025-04-15 09:10:28 -0600

    feat: add shared folder between host and vm

--- hosts/vm-aarch64.nix
+++ hosts/vm-aarch64.nix
@@ -60,6 +60,19 @@
     htop
     unzip
   ];  
+
+  fileSystems."/host" = {
+    fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
+    device = ".host:/";
+    options = [
+      "umask=22"
+      "uid=1000"
+      "gid=1000"
+      "allow_other"
+      "auto_unmount"
+      "defaults"
+    ];
+  };
   
   programs.zsh.shellInit = ''
     # Nix
