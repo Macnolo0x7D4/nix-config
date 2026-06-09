0528eb3838518e974c01c071d3e662bd6f59cc98
Manuel Díaz <diaz@macnolo.net>
2026-06-08 22:50:49 -0600

    feat: add new macOS apps; add darwin config

--- users/macnolo/darwin.nix
+++ users/macnolo/darwin.nix
@@ -15,15 +15,21 @@
     ];
 
     casks = [
-      "1password"
-      "1password-cli"
+      "brave-browser"
+      "ghostty"
       "obsidian"
       "google-chrome"
       "discord"
       "slack"
       "spotify"
+      "nordpass"
+      "alfred"
     ];
   };
 
   security.pam.services.sudo_local.touchIdAuth = true;
+
+
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 25;
+  system.defaults.NSGlobalDomain.KeyRepeat = 2;
 }
