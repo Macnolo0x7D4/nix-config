b988cc52b93af7bd0a7988fb67955c05ab5d08dd
Manuel Díaz <diaz@macnolo.net>
2026-06-13 10:37:34 -0600

    feat: upgrade to 26.06

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "Macnolo0x7D4's Nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -52,7 +52,7 @@
       user = "macnolo";
     };
     
-    darwinConfigurations."Macnolo-Air" = mkSystem "macbook-air-m1" {
+    darwinConfigurations."Manuels-MacBook-Air" = mkSystem "macbook-air-m1" {
       system = "aarch64-darwin";
       user = "macnolo";
       darwin = true;
